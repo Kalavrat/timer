f870507be7d054a9044513974d717548e66fa553
Kalavrat <97897883+Kalavrat@users.noreply.github.com>
2022-03-14 23:13:25 +0300

    Add files via upload

---
+++ timer/main.js
@@ -0,0 +1,22 @@
+// let btn = document.querySelector(".btn");
+// let inp = document.querySelector(".inp");
+// let div = document.querySelector(".div");
+// let timerid;
+// function timer() {
+//   if (!Number(inp.value)) {
+//     alert("Введите число");
+//     return;
+//   } else {
+//     clearInterval(timerid);
+//     div.textContent = inp.value;
+//     timerid = setInterval(() => {
+//       if (div.textContent == 0) {
+//         clearInterval(timerid);
+//       } else {
+//         div.textContent -= 1;
+//       }
+//     }, 1000);
+//   }
+// }
+
+// btn.addEventListener("click", timer);
